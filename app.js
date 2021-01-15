be3caf5aa85ff6dbab53b1cca0c67d8243caaad2
Denisse <denissefortanelli1402@gmail.com>
2021-01-15 08:58:10 -0600

    preparar para servidor

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express=require('express');
 const path=require('path');
 const app=express();
+const port=process.env.port || 3000;
 
-console.log(__dirname);
 
 //el sugundo public dice como se llama la carpeta
 //el primer public quiere decir como lo vas a encontrar
 app.use('/',express.static(path.join(__dirname,'/public')));
 
 
-app.listen(3000, ()=>{
- console.log("Servidor escuchado en el puerto 3000");
+app.listen(port, ()=>{
+ console.log("Servidor escuchado en el puerto " +port);
 } );
